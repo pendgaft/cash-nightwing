9bcc65828a3bb64782b7e00aa6f2b7753e4d384d
unknown <Nathan@WIN-L0A25LO1GAL.(none)>
2012-04-20 10:44:11 -0500

    fixed to work with comments

--- src/ASFileReader.java
+++ src/ASFileReader.java
@@ -10,7 +10,7 @@ public class ASFileReader {
 
 		try {
 			Scanner sc = new Scanner(file);
-			sc.useDelimiter("\\x7C|\\p{javaWhitespace}+"); // checks for | or whitespace
+			//sc.useDelimiter("\\x7C|\\p{javaWhitespace}+"); // checks for | or whitespace
 			
 			
 			int asn1;
@@ -18,29 +18,37 @@ public class ASFileReader {
 			AS as1;
 			AS as2;
 			int relation;
+			String line; // line read in by scanner
+			String delimiter = "\\x7C"; // code for | character
+			String[] splitList; // list created by split()
 
 			HashMap<Integer, AS> list = new HashMap<Integer, AS>();
 
-			while(sc.hasNextInt()) {
-				asn1 = sc.nextInt();
-				asn2 = sc.nextInt();
-				relation = sc.nextInt();
-				as1 = list.get(new Integer(asn1));
-				as2 = list.get(new Integer(asn2));
-				
-				if(as1 == null) {
-					as1 = new AS(asn1);
-					list.put(new Integer(asn1), as1);
-				}
-				
-				if(as2 == null) {
-					as2 = new AS(asn2);
-					list.put(new Integer(asn2), as2);
-				}
+			while(sc.hasNextLine()) {
+				line = sc.nextLine();
+				if(line.charAt(0) == '#') { // check if the line is a comment
+					continue;
+				} else {
+					splitList = line.split(delimiter);
+					asn1 = Integer.parseInt(splitList[0]);
+					asn2 = Integer.parseInt(splitList[1]);
+					relation = Integer.parseInt(splitList[2]);
+					as1 = list.get(new Integer(asn1));
+					as2 = list.get(new Integer(asn2));
 
-				as1.addRelation(as2, relation);
+					if(as1 == null) {
+						as1 = new AS(asn1);
+						list.put(new Integer(asn1), as1);
+					}
+
+					if(as2 == null) {
+						as2 = new AS(asn2);
+						list.put(new Integer(asn2), as2);
+					}
+
+					as1.addRelation(as2, relation);
+				}
 			}
-			
 			return list;
 		} catch (FileNotFoundException e) {	
 			e.printStackTrace();
