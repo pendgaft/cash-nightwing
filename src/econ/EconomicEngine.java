24f7644bd2fafee3ac8d77a9f43e22036d2bc861
pendgaft <schuch@cs.umn.edu>
2013-08-09 16:07:53 -0500

    functional, but missing a few key points, Economic Engine

--- src/econ/EconomicEngine.java
+++ src/econ/EconomicEngine.java
@@ -1,6 +1,9 @@
 package econ;
 
 import java.util.*;
+import java.io.*;
+
+import decoy.DecoyAS;
 
 import topo.AS;
 
@@ -9,14 +12,40 @@ public class EconomicEngine {
 	private HashMap<Integer, EconomicAgent> theTopo;
 	private HashMap<Integer, Double> cashForThisRound;
 
+	private BufferedWriter wardenOut;
+	private BufferedWriter transitOut;
+
 	private static final double TRAFFIC_UNIT_TO_MBYTES = 1.0;
 	private static final double COST_PER_MBYTE = 1.0;
-	
-	public EconomicEngine(HashMap<Integer, EconomicAgent> fullTopo) {
-		this.theTopo = fullTopo;
+
+	public EconomicEngine(HashMap<Integer, DecoyAS> activeMap, HashMap<Integer, DecoyAS> prunedMap) {
+		this.theTopo = new HashMap<Integer, EconomicAgent>();
+		this.cashForThisRound = new HashMap<Integer, Double>();
+
+		try {
+			this.wardenOut = new BufferedWriter(new FileWriter("logs/warden.log"));
+			this.transitOut = new BufferedWriter(new FileWriter("logs/transit.log"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		for (DecoyAS tAS : prunedMap.values()) {
+			this.theTopo
+					.put(tAS.getASN(), new TransitProvider(tAS, this.transitOut, TransitProvider.DECOY_STRAT.NEVER));
+		}
+		for (DecoyAS tAS : activeMap.values()) {
+			if (tAS.isWardenAS()) {
+				this.theTopo.put(tAS.getASN(), new WardenAgent(tAS, this.wardenOut));
+			} else {
+				this.theTopo.put(tAS.getASN(), new TransitProvider(tAS, this.transitOut,
+						TransitProvider.DECOY_STRAT.RAND));
+			}
+		}
 	}
 
 	public void driveEconomicTurn() {
+		System.out.println("Starting econ part");
 		this.resetForNewRound();
 		this.runMoneyTransfer();
 
@@ -49,16 +78,37 @@ public class EconomicEngine {
 		}
 	}
 
+	public void endSim() {
+		try {
+			this.wardenOut.close();
+			this.transitOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
 	private void runMoneyTransfer() {
 		for (Integer tASN : this.theTopo.keySet()) {
 			EconomicAgent tAgent = this.theTopo.get(tASN);
 			for (int tNeighbor : tAgent.getNeighbors()) {
-				
-				int relation = tAgent.getRelationship(tNeighbor);
+
+				int relation = 0;
+				try {
+					relation = tAgent.getRelationship(tNeighbor);
+				} catch (IllegalArgumentException e) {
+					if(tAgent.isPurged()){
+						relation = AS.CUSTOMER_CODE;
+					}else if(this.theTopo.get(tNeighbor).isPurged()){
+						relation = AS.PROIVDER_CODE;
+					} else{
+						throw e;
+					}
+				}
 				if (relation == AS.PEER_CODE) {
 					continue;
 				}
-				
+
 				double trafficVolume = tAgent.getTrafficOverLinkBetween(tNeighbor);
 				double scaleFactor = this.buildScaleFactor(tAgent, this.theTopo.get(tNeighbor), relation);
 				double moneyFlow = trafficVolume * scaleFactor;
