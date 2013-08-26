ecb9be16ad140800b740c591f73d8d55f9147899
pendgaft <schuch@cs.umn.edu>
2013-08-26 10:33:26 -0500

    minor changes in appearance to Constants file

--- src/sim/Constants.java
+++ src/sim/Constants.java
@@ -6,9 +6,13 @@ public class Constants {
 	public static final String IP_COUNT_FILE = "cash-nightwing/ip-count.csv";
 	public static final String SUPER_AS_FILE = "cash-nightwing/superAS.txt";
 	
-	//public static final String AS_REL_FILE = "bowenTest/as-rel-test.txt";
-	//public static final String IP_COUNT_FILE = "bowenTest/ip-count-test.csv";
-	//public static final String SUPER_AS_FILE = "bowenTest/superAS-test.txt";
+//	public static final String AS_REL_FILE = "topo1Test/as-rel.txt";
+//	public static final String IP_COUNT_FILE = "topo1Test/ip-count.txt";
+//	public static final String SUPER_AS_FILE = "topo1Test/superAS.txt";
+	
+//	public static final String AS_REL_FILE = "maxTest/5node-rel.txt";
+//	public static final String IP_COUNT_FILE = "maxTest/ip-count.csv";
+//	public static final String SUPER_AS_FILE = "maxTest/superAS-test.txt";
 	
 	public static final boolean DEBUG = false;
 	public static final int NTHREADS = 10;
