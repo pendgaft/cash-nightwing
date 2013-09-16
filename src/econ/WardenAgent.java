ef9240605ae8e03c717b22fef233154c4c7d9a0e
Max Schuchard <schuch@cs.umn.edu>
2013-09-16 15:22:07 -0500

    added logging for warden to add the AS reachability info in addition to the ip reachability

--- src/econ/WardenAgent.java
+++ src/econ/WardenAgent.java
@@ -13,7 +13,8 @@ public class WardenAgent extends EconomicAgent {
 	private HashMap<Integer, DecoyAS> activeTopo;
 	private HashMap<Integer, DecoyAS> prunedTopo;
 
-	public WardenAgent(DecoyAS parentObject, BufferedWriter log, HashMap<Integer, DecoyAS> activeTopo, HashMap<Integer, DecoyAS> prunedTopo) {
+	public WardenAgent(DecoyAS parentObject, BufferedWriter log, HashMap<Integer, DecoyAS> activeTopo,
+			HashMap<Integer, DecoyAS> prunedTopo) {
 		super(parentObject, log);
 		if (!parentObject.isWardenAS()) {
 			throw new IllegalArgumentException("Passed a non warden DecoyAS object to WardenAgent constructor.");
@@ -24,51 +25,60 @@ public class WardenAgent extends EconomicAgent {
 	}
 
 	/**
-	 * Current logging logs the fraction of IP ADDRESS SPACE that the warden has a clean path to
+	 * Current logging logs the fraction of IP ADDRESS SPACE that the warden has
+	 * a clean path to followed by the fraction of the AS SPACE that the warden
+	 * has a clean path to
 	 */
 	@Override
 	public void doRoundLogging() {
 		Set<Integer> decoySet = this.buildDecoySet();
 		double totalIPCount = 0.0;
 		double cleanIPCount = 0.0;
+		double totalASCount = 0.0;
+		double cleanASCount = 0.0;
 		int nullCount = 0;
-		
-		for(int tDest: this.activeTopo.keySet()){
-			if(tDest == this.parent.getASN()){
+
+		for (int tDest : this.activeTopo.keySet()) {
+			if (tDest == this.parent.getASN()) {
 				continue;
 			}
-			
+
 			BGPPath tPath = this.parent.getPath(tDest);
-			if(tPath == null){
+			if (tPath == null) {
 				nullCount++;
 				continue;
 			}
 			int ipCount = this.activeTopo.get(tDest).getIPCount();
-			if(!tPath.containsAnyOf(decoySet)){
+			if (!tPath.containsAnyOf(decoySet)) {
 				cleanIPCount += ipCount;
+				cleanASCount += 1;
 			}
 			totalIPCount += ipCount;
+			totalASCount += 1;
 		}
 
-		for(int tDest: this.prunedTopo.keySet()){
+		for (int tDest : this.prunedTopo.keySet()) {
 			List<Integer> hookASNs = new ArrayList<Integer>();
-			for(AS tParent: this.prunedTopo.get(tDest).getProviders()){
+			for (AS tParent : this.prunedTopo.get(tDest).getProviders()) {
 				hookASNs.add(tParent.getASN());
 			}
 			BGPPath tPath = this.parent.getPathToPurged(hookASNs);
-			if(tPath == null){
+			if (tPath == null) {
 				continue;
 			}
 			int ipCount = this.prunedTopo.get(tDest).getIPCount();
-			if(!tPath.containsAnyOf(decoySet)){
+			if (!tPath.containsAnyOf(decoySet)) {
 				cleanIPCount += ipCount;
+				cleanASCount += 1;
 			}
 			totalIPCount += ipCount;
+			totalASCount += 1;
 		}
-		
-		try{
-			this.logStream.write("" + this.parent.getASN() + "," + cleanIPCount / totalIPCount + "\n");
-		} catch(IOException e){
+
+		try {
+			this.logStream.write("" + this.parent.getASN() + "," + cleanIPCount / totalIPCount + "," + cleanASCount
+					/ totalASCount + "\n");
+		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
@@ -94,7 +104,7 @@ public class WardenAgent extends EconomicAgent {
 		 */
 	}
 
-	private Set<Integer> buildDecoySet(){
+	private Set<Integer> buildDecoySet() {
 		HashSet<Integer> decoySet = new HashSet<Integer>();
 		for (DecoyAS tAS : this.activeTopo.values()) {
 			if (tAS.isDecoy()) {
