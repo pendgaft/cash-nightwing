3b051af6b458dd8217e7876bd9bce37e414eb4a1
pendgaft <schuch@cs.umn.edu>
2014-01-30 12:51:53 -0600

    enhanced the apples to apples profit delta with some more stat generation

--- src/parsing/MiscParsing.java
+++ src/parsing/MiscParsing.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 
 import sim.Constants;
 import topo.ASTopoParser;
+import topo.ASRanker;
 import decoy.DecoyAS;
 import econ.EconomicEngine;
 
@@ -17,31 +18,31 @@ public class MiscParsing {
 	public static void main(String[] args) throws IOException {
 		MiscParsing.directAStoASComparison(args[0], args[1], args[2]);
 	}
-	
+
 	private static void buildFixedASSimList(int minCCSize, int numberOfASes, int numberOfRounds) throws IOException {
 		HashMap<Integer, DecoyAS> asMap = ASTopoParser.parseFile("as-rel.txt", "china-as.txt", "superAS.txt");
-		for(DecoyAS tAS: asMap.values()){
+		for (DecoyAS tAS : asMap.values()) {
 			EconomicEngine.buildIndividualCustomerCone(tAS);
 		}
-		
+
 		List<Integer> possSet = new ArrayList<Integer>();
-		for(DecoyAS tAS: asMap.values()){
-			if(tAS.getCustomerConeSize() >= minCCSize && !tAS.isWardenAS() && !tAS.isSuperAS()){
+		for (DecoyAS tAS : asMap.values()) {
+			if (tAS.getCustomerConeSize() >= minCCSize && !tAS.isWardenAS() && !tAS.isSuperAS()) {
 				possSet.add(tAS.getASN());
 			}
 		}
-		
-		if(possSet.size() < numberOfASes){
+
+		if (possSet.size() < numberOfASes) {
 			System.out.println("too few ASes (" + possSet.size() + ")");
 			return;
-		} else{
+		} else {
 			System.out.println("poss  ASes: " + possSet.size());
 		}
-		
+
 		BufferedWriter outBuff = new BufferedWriter(new FileWriter("drConfig.txt"));
-		for(int counter = 0; counter < numberOfRounds; counter++){
+		for (int counter = 0; counter < numberOfRounds; counter++) {
 			Collections.shuffle(possSet);
-			for(int numberCounter = 0; numberCounter < numberOfASes; numberCounter++){
+			for (int numberCounter = 0; numberCounter < numberOfASes; numberCounter++) {
 				outBuff.write("" + possSet.get(numberCounter) + "\n");
 			}
 			outBuff.write(" \n");
@@ -152,7 +153,7 @@ public class MiscParsing {
 		return results;
 	}
 
-	private static void directAStoASComparison(String fileA, String fileB, String outFile) throws IOException {
+	private static void directAStoASComparison(String fileA, String fileB, String outFileBase) throws IOException {
 		HashMap<Integer, HashMap<Integer, Double>> roundADeltas = MiscParsing.computeProfitDeltas(fileA, true, 3, true);
 		HashMap<Integer, HashMap<Integer, Double>> roundBDeltas = MiscParsing.computeProfitDeltas(fileB, true, 3, true);
 
@@ -175,19 +176,67 @@ public class MiscParsing {
 					System.err.println("the two AS sets don't match up!");
 					return;
 				}
-				roundChanges.put(tASN, (bValue - aValue) / aValue * -100.0);
+				roundChanges.put(tASN, (bValue - aValue) / aValue * 100.0);
 			}
 
 			change.put(roundKey, roundChanges);
 		}
-		
+
 		HashMap<Integer, List<Double>> outMap = new HashMap<Integer, List<Double>>();
-		for(int tRoundKey: change.keySet()){
+		for (int tRoundKey : change.keySet()) {
 			List<Double> roundDeltas = new ArrayList<Double>();
 			roundDeltas.addAll(change.get(tRoundKey).values());
 			outMap.put(tRoundKey, roundDeltas);
 		}
-		MaxParser.printCDF(outMap, outFile);
+		MaxParser.printCDF(outMap, outFileBase + "-splitDeltaCDF.csv");
+		HashMap<Integer, List<Double>> compressedMap = new HashMap<Integer, List<Double>>();
+		List<Double> compressedValues = new ArrayList<Double>();
+		for (List<Double> tList : outMap.values()) {
+			compressedValues.addAll(tList);
+		}
+		compressedMap.put(0, compressedValues);
+		MaxParser.printCDF(compressedMap, outFileBase + "-compressedDeltaCDF.csv");
+
+		/*
+		 * Split the CDFs up by their initial losses, see if we can draw
+		 * conclusions, step one, figure out which ASes belong in which slots
+		 * based on their FIRST ROUND LOSSES
+		 */
+		int numberOfBuckets = 5;
+		HashMap<Integer, List<Integer>> sliceNodes = new HashMap<Integer, List<Integer>>();
+		List<ASRanker> values = new ArrayList<ASRanker>();
+		HashMap<Integer, Double> sampleMap = roundADeltas.get(0);
+		for (int tASN : sampleMap.keySet()) {
+			values.add(new ASRanker(tASN, sampleMap.get(tASN)));
+		}
+		Collections.sort(values);
+		int sliceSize = (int) Math.floor((double) values.size() / (double) numberOfBuckets);
+		int pos = 0;
+		for (int counter = 0; counter < numberOfBuckets; counter++) {
+			List<Integer> currentSliceMembers = new ArrayList<Integer>(sliceSize);
+			for (int innerCounter = 0; innerCounter < sliceSize; innerCounter++) {
+				currentSliceMembers.add(values.get(pos).getASN());
+				pos++;
+			}
+			sliceNodes.put(counter, currentSliceMembers);
+		}
+		while (pos < values.size()) {
+			sliceNodes.get(numberOfBuckets - 1).add(values.get(pos).getASN());
+			pos++;
+		}
+
+		/*
+		 * Populate the slices with the actual deltas and output
+		 */
+		HashMap<Integer, List<Double>> sliceValues = new HashMap<Integer, List<Double>>();
+		for (int tSlice : sliceNodes.keySet()) {
+			List<Double> sliceValueList = new ArrayList<Double>(sliceSize);
+			for (int tASN : sliceNodes.get(tSlice)) {
+				sliceValueList.add(change.get(0).get(tASN));
+			}
+			sliceValues.put(tSlice, sliceValueList);
+		}
+		MaxParser.printCDF(sliceValues, outFileBase + "-sliceDeltaCDF.csv");
 	}
 
 }
