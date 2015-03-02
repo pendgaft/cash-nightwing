144462273b7c744461b83b6171e8449fbf3b3438
pendgaft <m.schuchard@gmail.com>
2015-03-02 16:08:50 -0600

    updated contsants to reference the 2015 as rel file

--- src/sim/Constants.java
+++ src/sim/Constants.java
@@ -5,7 +5,7 @@ import topo.AS;
 
 public class Constants {
 
-	public static final String AS_REL_FILE = "cash-nightwing/realTopo/whole-internet-20150101-rel.txt";
+	public static final String AS_REL_FILE = "cash-nightwing/realTopo/20150201.as-rel.txt";
 	public static final String IP_COUNT_FILE = "cash-nightwing/realTopo/whole-internet-20150101-ip.txt";
 	public static final String SUPER_AS_FILE = "cash-nightwing/superAS.txt";
 	
