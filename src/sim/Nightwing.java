b2df23d3adf7c8153fbcfcd2d1e5a03fb46e9264
pendgaft <m.schuchard@gmail.com>
2016-02-03 13:27:02 -0600

    Fixed error where partial defection and defection flags were out of step in vs mode.

--- src/sim/Nightwing.java
+++ src/sim/Nightwing.java
@@ -220,7 +220,7 @@ public class Nightwing implements Runnable {
 					this.myArgs.getBoolean("defection"));
 		} else if (this.myMode == Nightwing.SimMode.VS) {
 			this.econManager.manageDictatedDRSim(this.myArgs.getString("deployers"),
-					this.myArgs.getBoolean("defection"), true);
+					this.myArgs.getBoolean("defection"), this.myArgs.getBoolean("defection"));
 		} else if (this.myMode == Nightwing.SimMode.RANDOMNUMBER) {
 			this.econManager.manageRandomDeploySizeSim(Constants.DEFAULT_DEPLOY_START, Constants.DEFAULT_DEPLOY_STOP,
 					Constants.DEFAULT_DEPLOY_STEP, Constants.RANDOM_SAMPLE_COUNT, Constants.DEFAULT_FIGURE_OF_MERIT);
